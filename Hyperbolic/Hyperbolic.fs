e9be958f6d1d8dfa885ce9fdcb8d46dfb192dbbf
heidisu <heidimork@yahoo.no>
2018-06-09 01:07:27 +0200

    Add drawings with tesselation lines

--- Hyperbolic/Hyperbolic.fs
+++ Hyperbolic/Hyperbolic.fs
@@ -9,7 +9,12 @@ type Point = Point of float * float
 type Action = Draw of Point * Point
 type Instructions = Action list
 type Adjacency = Vertex | Edge
+type ImageProperties = 
+        { Size : int
+          CircleRadius : float }
 
+let imageProperties = { Size = 700
+                        CircleRadius = 300.0 }
 let [<Literal>] P = 6
 let [<Literal>] Q = 4
 
@@ -41,13 +46,15 @@ let transformPoint (t : Matrix<float>) (Point(x, y)) =
 
 let drawPolygonPattern (graphics : Graphics) (t : Matrix<float>) (instructions : Instructions) = 
     let pen = new Pen(Color.Black)
-    let center = 400.0
-    let radius = 300.0
+    let center = (float) imageProperties.Size / 2.0
+    let radius = imageProperties.CircleRadius
     let scale (x, y) = (x * radius + center, y * radius + center)
-    let rectangle = new Rectangle(
-                            new Drawing.Point(int (center-radius), int (center - radius)), 
-                            new Size(new  Drawing.Point(int (2.0 * radius), int (2.0 * radius))))
+    
+    let rectangle = new RectangleF(
+                            new PointF(float32 (center - radius), float32 (center - radius)), 
+                            new SizeF(PointF(float32 (2.0 * radius), float32 (2.0 * radius))))
     graphics.DrawEllipse(pen, rectangle)
+    
     instructions
     |> List.iter (fun instr -> match instr with
                                | Draw (p1, p2) ->   let p1x, p1y = p1 |> transformPoint t |> scale
@@ -81,7 +88,8 @@ let rec replicate graphics transform layers adjacency instructions =
         | Vertex -> replicateEdges (P - 2) (transform * rotate2P)
 
 let drawImage filename layers instructions = 
-    let image = new Bitmap(1000, 1000)
+    let size = imageProperties.Size
+    let image = new Bitmap(size, size)
     let graphics = Graphics.FromImage image
 
     let rec drawQpolygon count rotateVertex = 
@@ -99,7 +107,39 @@ let drawImage filename layers instructions =
             drawPpolygon (count - 1) (rotateCenter * rotateP)
 
     drawPolygonPattern graphics identity instructions
-    drawPpolygon P identity    
+    drawPpolygon P identity  
+    image.RotateFlip RotateFlipType.RotateNoneFlipY  
+    image.Save filename
+
+let drawWithTesselationLines filename transform instructions = 
+    let size = imageProperties.Size
+    let image = new Bitmap(size, size)
+    let graphics = Graphics.FromImage image
+    let center = (float) size/2.0
+    let radius = imageProperties.CircleRadius
+    
+    let linePen = new Pen(Color.Green)
+    linePen.DashPattern <-[|float32 4.0; float32 4.0|]
+    let circlePen = new Pen(Color.Purple)
+
+  
+    let scale (x, y) = new PointF(float32 (x * radius + center), float32 (y * radius + center))
+
+    [1.0 .. fP]
+    |> Seq.map (fun n -> new RectangleF(
+                                scale(Math.Sqrt(2.0)*Math.Cos(2.0*Math.PI*n/fP) - 1.0, Math.Sqrt(2.0)*Math.Sin(2.0*Math.PI*n/fP) - 1.0),
+                                new SizeF (new PointF (float32 (2.0 * radius), float32 (2.0 * radius)))))
+    |> Seq.iter (fun rectangle -> graphics.DrawEllipse(circlePen, rectangle))
+
+    [1.0 .. fP]
+    |> Seq.iter(fun n -> graphics.DrawLine(
+                                        linePen, 
+                                        scale(Math.Cos(Math.PI*n/fP), Math.Sin(Math.PI*n/fP)), 
+                                        scale(Math.Cos(Math.PI*(n + fP)/fP), Math.Sin(Math.PI*(n + fP)/fP))))
+
+    drawPolygonPattern graphics identity instructions 
+    drawPolygonPattern graphics transform instructions 
+    image.RotateFlip RotateFlipType.RotateNoneFlipY
     image.Save filename
 
 [<EntryPoint>]
@@ -107,21 +147,26 @@ let main _ =
     let c = 0.4
     let d = 0.05
     let rotatePi idx frac c = Point(c * Math.Cos(Math.PI*idx/frac), c * Math.Sin(Math.PI*idx/frac))
-    let rotPi6 idx = rotatePi idx 6.0
-    let rotPi12 idx = rotatePi idx 12.0
+    let rotPiP idx = rotatePi idx fP
+    let rotPi2P idx = rotatePi idx (2.0 * fP)
 
     let polygonInstuctions = 
-        seq { 0.0 .. 11.0}
+        seq {1.0 .. (2.0 * fP)}
         |> Seq.toList
-        |> List.map (fun idx -> Draw(rotPi6 idx c, rotPi6 (idx + 1.0) c))
+        |> List.map (fun idx -> Draw(rotPiP idx c, rotPiP (idx + 1.0) c))
     
     let lineInstructions = 
-        seq { 1.0 .. 2.0 .. 23.0}
+        seq {1.0 .. 2.0 .. (4.0 * fP - 1.0)}
         |> Seq.toList
-        |> List.map (fun idx -> Draw(rotPi12 idx d, rotPi12 idx c))
+        |> List.map (fun idx -> Draw(rotPi2P idx d, rotPi2P idx c))
 
     drawImage "polygons.png" 4 polygonInstuctions
     drawImage "lines.png" 4 lineInstructions
-   
 
+    drawWithTesselationLines "edgebisector.png" reflectEdgeBisector [Draw(rotPi2P 1.0 d, rotPi2P 1.0 c)]
+    drawWithTesselationLines "hypotenuse.png" reflectHypotenuse [Draw(rotPi2P 1.0 d, rotPi2P 1.0 c)]
+    drawWithTesselationLines "pgonedge.png" reflectPgonEdge [Draw(rotPi2P 1.0 d, rotPi2P 1.0 c)]
+    drawWithTesselationLines "rotP.png" rotateP [Draw(rotPi2P 1.0 d, rotPi2P 1.0 c)]
+    drawWithTesselationLines "rotQ.png" rotateQ [Draw(rotPi2P 1.0 d, rotPi2P 1.0 c)]
+    
     0
